e8eae19856f01babac92bab7875caf8ad9f0a235
Ryo Omori <ryo763783@gmail.com>
2021-04-11 13:24:23 +0900

    fix

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from search_console_api_client import SearchConsoleApiClient
 
 
 param_date = '2021-02-17'
-client = SearchConsoleApiClient('https://job.newspicks.com/')
+client = SearchConsoleApiClient('url')
 data = client.query_search_analytics(datetime.strptime(param_date, '%Y-%m-%d'), ['query', 'page'])
 data.to_csv('{}.csv'.format(param_date), index=False)
